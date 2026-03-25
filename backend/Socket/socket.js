d6ab37ff9c232c1625b553d1e821f87294e0781e
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2026-03-25 14:18:53 +0530

    Fix CORS configuration in socket.js

--- backend/Socket/socket.js
+++ backend/Socket/socket.js
@@ -9,7 +9,8 @@ const app=express();
 const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:['http://localhost:5173'],
+        // origin:['http://localhost:5173'],
+           origin:['http://localhost:5173'],
         methods:["Get","Post"],
         credentials:true
     }
