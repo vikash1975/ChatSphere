51ff4c61f46ba6c01042fd7b22019a9f191e68e3
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2026-03-25 14:19:37 +0530

    Update CORS origin for production deployment

--- backend/Socket/socket.js
+++ backend/Socket/socket.js
@@ -10,7 +10,7 @@ const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
         // origin:['http://localhost:5173'],
-           origin:['http://localhost:5173'],
+           origin:['https://chatsphere-4pmw.onrender.com'],
         methods:["Get","Post"],
         credentials:true
     }
