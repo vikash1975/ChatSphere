ca93c07798823343e676693af966f51f746a3e3c
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2026-03-25 14:21:29 +0530

    Update socket connection URL to production

--- frontend/src/redux/SocketContext.jsx
+++ frontend/src/redux/SocketContext.jsx
@@ -4,7 +4,7 @@
 
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000', {
+const socket = io('https://chatsphere-backend-ejqy.onrender.com', {
   autoConnect: false,
   // withCredentials: true
 });
